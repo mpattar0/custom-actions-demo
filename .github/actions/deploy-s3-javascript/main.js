f110be41ba15eefeb5e94186387f39d452a84738
mpattar0 <mpattar0@in.ibm.com>
2026-04-02 09:45:18 +0530

    fix: update main.js to retrieve input values for S3 deployment

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,7 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run() {
-    core.notice('Hello from my custom JavaScript action!');
+    // 01] Get some input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: false });
+    const distFolder = core.getInput('dist-folder', { required: true });
+    //02] Uploade files
+    core.notice(`Deploying to bucket: ${bucket} in region: ${bucketRegion} from folder: ${distFolder}`);
 }
-
 run();
